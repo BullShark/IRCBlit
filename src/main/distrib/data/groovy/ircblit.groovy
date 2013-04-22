32af5ef24d6cd142b1bf6d829a6e691fed94c550
BullShark <goodbye300@aim.com>
2013-04-22 17:25:03 -0500

    Added javadoc comments

--- src/main/distrib/data/groovy/ircblit.groovy
+++ src/main/distrib/data/groovy/ircblit.groovy
@@ -1,4 +1,5 @@
 package expectusafterlun.ch.irc
+
 import com.gitblit.GitBlit
 import com.gitblit.Keys
 import com.gitblit.models.RepositoryModel
@@ -10,6 +11,22 @@ import org.eclipse.jgit.transport.ReceiveCommand
 import org.eclipse.jgit.transport.ReceiveCommand.Result
 import org.slf4j.Logger
 
-class ircblit {
+/**
+ * Sample Gitblit Post-Receive Hook: ircblit
+ *
+ * Bound Variables:
+ *  gitblit			Gitblit Server	 			com.gitblit.GitBlit
+ *  repository		Gitblit Repository			com.gitblit.models.RepositoryModel
+ *  receivePack		JGit Receive Pack			org.eclipse.jgit.transport.ReceivePack
+ *  user			Gitblit User				com.gitblit.models.UserModel
+ *  commands		JGit commands 				Collection<org.eclipse.jgit.transport.ReceiveCommand>
+ *	url				Base url for Gitblit		String
+ *  logger			Logs messages to Gitblit 	org.slf4j.Logger
+ *  clientLogger	Logs messages to Git client	com.gitblit.utils.ClientLogger
+ *
+ * Accessing Gitblit Custom Fields:
+ *   def myCustomField = repository.customFields.myCustomField
+ */
 
-}
+// Indicate we have started the script
+logger.info("IRCBlit hook triggered by ${user.username} for ${repository.name}")
