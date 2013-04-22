ff25fa6557ef5b65750e7335498293d621f5bb64
BullShark <goodbye300@aim.com>
2013-04-21 19:14:21 -0500

    Project changed to include all of Gitblit's source

---
+++ src/main/distrib/data/groovy/ircblit.groovy
@@ -0,0 +1,15 @@
+package expectusafterlun.ch.irc
+import com.gitblit.GitBlit
+import com.gitblit.Keys
+import com.gitblit.models.RepositoryModel
+import com.gitblit.models.UserModel
+import com.gitblit.utils.JGitUtils
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.transport.ReceiveCommand
+import org.eclipse.jgit.transport.ReceiveCommand.Result
+import org.slf4j.Logger
+
+class ircblit {
+
+}
