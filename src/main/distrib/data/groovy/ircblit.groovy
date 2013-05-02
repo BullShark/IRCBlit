81d27f8d4e00905f6f39e37a7db38a761a0cdcc5
BullShark <christopher.lemire@gmail.com>
2013-05-01 20:52:53 -0500

    Reverting code back to a working revision

--- src/main/distrib/data/groovy/ircblit.groovy
+++ src/main/distrib/data/groovy/ircblit.groovy
@@ -19,10 +19,7 @@ import com.gitblit.Keys;
 import com.gitblit.models.RepositoryModel;
 import com.gitblit.models.UserModel;
 import com.gitblit.utils.JGitUtils;
-
-import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.patch.FileHeader;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.transport.ReceiveCommand;
 import org.eclipse.jgit.transport.ReceiveCommand.Result;
@@ -58,30 +55,8 @@ import java.net.SocketException;
  *   def myCustomField = repository.customFields.myCustomField
  */
 
-com.gitblit.models.UserModel userModel = user
-
 // Indicate we have started the script
 logger.info("IRCBlit hook triggered by ${user.username} for ${repository.name}");
-
-//Repository r = gitblit.getRepository(repository.name)
-
-// reuse existing repository config settings, if available
-//Config config = r.getConfig()
-// define the summary and commit urls
-def repo = repository.name
-def summaryUrl = url + "/summary?r=$repo"
-def baseCommitUrl = url + "/commit?r=$repo&h="
-def baseBlobDiffUrl = url + "/blobdiff/?r=$repo&h="
-def baseCommitDiffUrl = url + "/commitdiff/?r=$repo&h="
-def forwardSlashChar = gitblit.getString(Keys.web.forwardSlashCharacter, '/')
-
-if (gitblit.getBoolean(Keys.web.mountParameters, true)) {
-	repo = repo.replace('/', forwardSlashChar).replace('/', '%2F')
-	summaryUrl = url + "/summary/$repo"
-	baseCommitUrl = url + "/commit/$repo/"
-	baseBlobDiffUrl = url + "/blobdiff/$repo/"
-	baseCommitDiffUrl = url + "/commitdiff/$repo/"
-}
 
 class IRCBlit {
 	// Create global variables
@@ -102,10 +77,34 @@ class IRCBlit {
 	def joined;
 
 	/**
-	 * Gives all the global variables default values
-	 * @param logger
+	 * The constructor calls many helper methods
+	 * From setting up the irc connection to closing the connection
+	 * @param logger Used for logging info messages to Apache Tomcat's server logs
 	 */
 	IRCBlit(logger) {
+		initialize(logger);
+		createIRCSocket();
+		createIOStreams();
+		createReceivedThread()
+		sendNickAndUserMessages();
+		waitFor001();
+		joinChannel();
+		waitForChannelJoined();
+		gitBlitChannel();
+		// Send a test message to the chan
+		msgChannel(chan, ".wr");
+		msgChannel(chan, "ftl");
+		// Send a test notice to the chan
+		noticeChannel(chan, "Hello ${chan}");
+		gitBlitChannel();
+		quitAndCloseStreams();
+	}
+
+	/**
+	 * Gives all the global variables default values
+	 * @return
+	 */
+	def initialize(logger) {
 		server = "frequency.windfyre.net";
 		port = 6667;
 		chan = "#blackhats";
@@ -121,29 +120,6 @@ class IRCBlit {
 		received001 = false;
 		joined = false;
 		//quitMsg = "GitBlit Service Hook by BullShark"
-	}
-
-	/**
-	 * The constructor calls many helper methods
-	 * From setting up the irc connection to closing the connection
-	 * @param logger Used for logging info messages to Apache Tomcat's server logs
-	 */
-	def start(content) {
-		createIRCSocket();
-		createIOStreams();
-		createReceivedThread()
-		sendNickAndUserMessages();
-		waitFor001();
-		joinChannel();
-		waitForChannelJoined();
-		gitBlitChannel(content);
-		// Send a test message to the chan
-		msgChannel(chan, ".wr");
-		msgChannel(chan, "ftl");
-		// Send a test notice to the chan
-		noticeChannel(chan, "Hello ${chan}");
-		gitBlitChannel();
-		quitAndCloseStreams();
 	}
 
 	/**
@@ -277,7 +253,7 @@ class IRCBlit {
 	/**
 	 * 
 	 */
-	def gitBlitChannel(content) {
+	def gitBlitChannel() {
 		/*
 		 * Produce similar messages to these here in this method:
 		 * 
@@ -303,8 +279,7 @@ class IRCBlit {
 		 * 8 yellow 9 light green 10 dark teal 11 light teal
 		 * 12 light blue 13 light purple 14 dark gray 15 light gray
 		 */
-
-		noticeChannel(chan, content);
+		
 	}
 
 	/**
@@ -406,367 +381,9 @@ class IRCBlit {
 	def quitAndCloseStreams() {
 		quitAndCloseStreams(true);
 	}
-
-	/*
-	 * Used for Git Summary Message
-	 */
-	Repository repository
-	def url
-	def baseCommitUrl
-	def baseCommitDiffUrl
-	def baseBlobDiffUrl
-	def mountParameters
-	def forwardSlashChar
-	def includeGravatar
-	def shortCommitIdLength
-	def commitCount = 0
-	def commands
-	def writer = new StringWriter();
-	def builder = new MarkupBuilder(writer)
-
-	def writeStyle() {
-		builder.style(type:"text/css", '''
-    .table td {
-        vertical-align: middle;
-    }
-    tr.noborder td {
-        border: none;
-        padding-top: 0px;
-    }
-    .gravatar-column {
-        width: 5%; 
-    }
-    .author-column {
-        width: 20%; 
-    }
-    .commit-column {
-        width: 5%; 
-    }
-    .status-column {
-        width: 10%;
-    }
-    .table-disable-hover.table tbody tr:hover td,
-    .table-disable-hover.table tbody tr:hover th {
-        background-color: inherit;
-    }
-    .table-disable-hover.table-striped tbody tr:nth-child(odd):hover td,
-    .table-disable-hover.table-striped tbody tr:nth-child(odd):hover th {
-      background-color: #f9f9f9;
-    }
-    ''')
-	}
-
-	def writeBranchTitle(type, name, action, number) {
-		builder.div('class' : 'pageTitle') {
-			builder.span('class':'project') {
-				mkp.yield "$type "
-				span('class': 'repository', name )
-				if (number > 0) {
-					mkp.yield " $action ($number commits)"
-				} else {
-					mkp.yield " $action"
-				}
-			}
-		}
-	}
-
-	def writeBranchDeletedTitle(type, name) {
-		builder.div('class' : 'pageTitle', 'style':'color:red') {
-			builder.span('class':'project') {
-				mkp.yield "$type "
-				span('class': 'repository', name )
-				mkp.yield " deleted"
-			}
-		}
-	}
-
-	def commitUrl(RevCommit commit) {
-		"${baseCommitUrl}$commit.id.name"
-	}
-
-	def commitDiffUrl(RevCommit commit) {
-		"${baseCommitDiffUrl}$commit.id.name"
-	}
-
-	def encoded(String path) {
-		path.replace('/', forwardSlashChar).replace('/', '%2F')
-	}
-
-	def blobDiffUrl(objectId, path) {
-		if (mountParameters) {
-			// REST style
-			"${baseBlobDiffUrl}${objectId.name()}/${encoded(path)}"
-		} else {
-			"${baseBlobDiffUrl}${objectId.name()}&f=${path}"
-		}
-
-	}
-
-	def writeCommitTable(commits, includeChangedPaths=true) {
-		// Write commits table
-		builder.table('class':"table table-disable-hover") {
-			thead {
-				tr {
-					th(colspan: includeGravatar ? 2 : 1, "Author")
-					th( "Commit" )
-					th( "Message" )
-				}
-			}
-			tbody() {
-
-				// Write all the commits
-				for (commit in commits) {
-					writeCommit(commit)
-
-					if (includeChangedPaths) {
-						// Write detail on that particular commit
-						tr('class' : 'noborder') {
-							td (colspan: includeGravatar ? 3 : 2)
-							td (colspan:2) { writeStatusTable(commit) }
-						}
-					}
-				}
-			}
-		}
-	}
-
-	def writeCommit(commit) {
-		def abbreviated = repository.newObjectReader().abbreviate(commit.id, shortCommitIdLength).name()
-		def author = commit.authorIdent.name
-		def email = commit.authorIdent.emailAddress
-		def message = commit.shortMessage
-		builder.tr {
-			if (includeGravatar) {
-				td('class':"gravatar-column") {
-					img(src:gravatarUrl(email), 'class':"gravatar")
-				}
-			}
-			td('class':"author-column", author)
-			td('class':"commit-column") {
-				a(href:commitUrl(commit)) {
-					span('class':"label label-info",  abbreviated )
-				}
-			}
-			td {
-				mkp.yield message
-				a('class':'link', href:commitDiffUrl(commit), " [commitdiff]" )
-			}
-		}
-	}
-
-	def writeStatusLabel(style, tooltip) {
-		builder.span('class' : style,  'title' : tooltip )
-	}
-
-	def writeAddStatusLine(ObjectId id, FileHeader header) {
-		builder.td('class':'changeType') { writeStatusLabel("addition", "addition") }
-		builder.td {
-			a(href:blobDiffUrl(id, header.newPath), header.newPath)
-		}
-	}
-
-	def writeCopyStatusLine(ObjectId id, FileHeader header) {
-		builder.td('class':'changeType') { writeStatusLabel("rename", "rename") }
-		builder.td() {
-			a(href:blobDiffUrl(id, header.newPath), header.oldPath + " copied to " + header.newPath)
-		}
-	}
-
-	def writeDeleteStatusLine(ObjectId id, FileHeader header) {
-		builder.td('class':'changeType') { writeStatusLabel("deletion", "deletion") }
-		builder.td() {
-			a(href:blobDiffUrl(id, header.oldPath), header.oldPath)
-		}
-	}
-
-	def writeModifyStatusLine(ObjectId id, FileHeader header) {
-		builder.td('class':'changeType') { writeStatusLabel("modification", "modification") }
-		builder.td() {
-			a(href:blobDiffUrl(id, header.oldPath), header.oldPath)
-		}
-	}
-
-	def writeRenameStatusLine(ObjectId id, FileHeader header) {
-		builder.td('class':'changeType') { writeStatusLabel("rename", "rename") }
-		builder.td() {
-			mkp.yield header.oldPath
-			mkp.yieldUnescaped "<b> -&rt; </b>"
-			a(href:blobDiffUrl(id, header.newPath),  header.newPath)
-		}
-	}
-
-	def writeStatusLine(ObjectId id, FileHeader header) {
-		builder.tr {
-			switch (header.changeType) {
-				case ChangeType.ADD:
-					writeAddStatusLine(id, header)
-					break;
-				case ChangeType.COPY:
-					writeCopyStatusLine(id, header)
-					break;
-				case ChangeType.DELETE:
-					writeDeleteStatusLine(id, header)
-					break;
-				case ChangeType.MODIFY:
-					writeModifyStatusLine(id, header)
-					break;
-				case ChangeType.RENAME:
-					writeRenameStatusLine(id, header)
-					break;
-			}
-		}
-	}
-
-	def writeStatusTable(RevCommit commit) {
-		DiffFormatter formatter = new DiffFormatter(DisabledOutputStream.INSTANCE)
-		formatter.setRepository(repository)
-		formatter.setDetectRenames(true)
-		formatter.setDiffComparator(RawTextComparator.DEFAULT);
-
-		def diffs
-		RevWalk rw = new RevWalk(repository)
-		if (commit.parentCount > 0) {
-			RevCommit parent = rw.parseCommit(commit.parents[0].id)
-			diffs = formatter.scan(parent.tree, commit.tree)
-		} else {
-			diffs = formatter.scan(new EmptyTreeIterator(),
-					new CanonicalTreeParser(null, rw.objectReader, commit.tree))
-		}
-		rw.dispose()
-		// Write status table
-		builder.table('class':"plain") {
-			tbody() {
-				for (DiffEntry entry in diffs) {
-					FileHeader header = formatter.toFileHeader(entry)
-					writeStatusLine(commit.id, header)
-				}
-			}
-		}
-	}
-
-
-	def md5(text) {
-
-		def digest = MessageDigest.getInstance("MD5")
-
-		//Quick MD5 of text
-		def hash = new BigInteger(1, digest.digest(text.getBytes()))
-				.toString(16)
-				.padLeft(32, "0")
-		hash.toString()
-	}
-
-	def gravatarUrl(email) {
-		def cleaned = email.trim().toLowerCase()
-		"http://www.gravatar.com/avatar/${md5(cleaned)}?s=30"
-	}
-
-	def writeNavbar() {
-		builder.div('class':"navbar navbar-fixed-top") {
-			div('class':"navbar-inner") {
-				div('class':"container") {
-					a('class':"brand", href:"${url}", title:"GitBlit") {
-						img(src:"${url}/gitblt_25_white.png",
-						width:"79",
-						height:"25",
-						'class':"logo")
-					}
-				}
-			}
-		}
-	}
-
-	def write() {
-		builder.html {
-			head {
-				link(rel:"stylesheet", href:"${url}/bootstrap/css/bootstrap.css")
-				link(rel:"stylesheet", href:"${url}/gitblit.css")
-				link(rel:"stylesheet", href:"${url}/bootstrap/css/bootstrap-responsive.css")
-				writeStyle()
-			}
-			body {
-
-				writeNavbar()
-
-				div('class':"container") {
-
-					for (command in commands) {
-						def ref = command.refName
-						def refType = 'Branch'
-						if (ref.startsWith('refs/heads/')) {
-							ref  = command.refName.substring('refs/heads/'.length())
-						} else if (ref.startsWith('refs/tags/')) {
-							ref  = command.refName.substring('refs/tags/'.length())
-							refType = 'Tag'
-						}
-
-						switch (command.type) {
-							case ReceiveCommand.Type.CREATE:
-								def commits = JGitUtils.getRevLog(repository, command.oldId.name, command.newId.name).reverse()
-								commitCount += commits.size()
-								if (refType == 'Branch') {
-									// new branch
-									writeBranchTitle(refType, ref, "created", commits.size())
-									writeCommitTable(commits, true)
-								} else {
-									// new tag
-									writeBranchTitle(refType, ref, "created", 0)
-									writeCommitTable(commits, false)
-								}
-								break
-							case ReceiveCommand.Type.UPDATE:
-								def commits = JGitUtils.getRevLog(repository, command.oldId.name, command.newId.name).reverse()
-								commitCount += commits.size()
-							// fast-forward branch commits table
-							// Write header
-								writeBranchTitle(refType, ref, "updated", commits.size())
-								writeCommitTable(commits)
-								break
-							case ReceiveCommand.Type.UPDATE_NONFASTFORWARD:
-								def commits = JGitUtils.getRevLog(repository, command.oldId.name, command.newId.name).reverse()
-								commitCount += commits.size()
-							// non-fast-forward branch commits table
-							// Write header
-								writeBranchTitle(refType, ref, "updated [NON fast-forward]", commits.size())
-								writeCommitTable(commits)
-								break
-							case ReceiveCommand.Type.DELETE:
-							// deleted branch/tag
-								writeBranchDeletedTitle(refType, ref)
-								break
-							default:
-								break
-						}
-					}
-				}
-			}
-		}
-		writer.toString()
-	}
 }
 
-def IRCBlit = new IRCBlit(logger);
-IRCBlit.repository = r
-IRCBlit.baseCommitUrl = baseCommitUrl
-IRCBlit.baseBlobDiffUrl = baseBlobDiffUrl
-IRCBlit.baseCommitDiffUrl = baseCommitDiffUrl
-IRCBlit.forwardSlashChar = forwardSlashChar
-IRCBlit.commands = commands
-IRCBlit.url = url
-IRCBlit.mountParameters = GitBlit.getBoolean(Keys.web.mountParameters, true)
-IRCBlit.includeGravatar = GitBlit.getBoolean(Keys.web.allowGravatar, true)
-IRCBlit.shortCommitIdLength = GitBlit.getInteger(Keys.web.shortCommitIdLength, 8)
-
-IRCBlit.start(IRCBlit.write());
-
-// close the repository reference
-r.close()
-
-// tell Gitblit to send the message (Gitblit filters duplicate addresses)
-//def repositoryName = repository.name.substring(0, repository.name.length() - 4)
-//gitblit.sendHtmlMail("${emailprefix} ${userModel.displayName} pushed ${mailWriter.commitCount} commits => $repositoryName",
-//					 content,
-//					 toAddresses)
+new IRCBlit(logger);
 
 //TODO Get Info by Accessing Gitblit Custom Fields
 //TODO And if the fields do not exist, use some defaults
